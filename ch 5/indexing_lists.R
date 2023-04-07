65de9c8d5d7bed9c0809deedc1c5babdbbfebe67
Aryan Sethi <aryan.rdps@gmail.com>
2023-04-07 20:47:19 +0530

    Added indexing list

---
+++ ch 5/indexing_lists.R
@@ -0,0 +1,43 @@
+l <- list(
+    first = 1,
+    second = 2,
+    third = list(
+        alpha = 3.1,
+        beta = 3.2
+    )
+)
+
+#indexing like before
+
+# [] Returns list
+l[1:2]
+l[-3]
+l[c("first", "second")]
+l[c(TRUE, TRUE, FALSE)]
+
+# [[]] actually returns list elements but for single +ve idx/single name
+print("other")
+l[[3]]
+l[["third"]]
+
+# check
+is.list(l[1])
+is.list(l[[1]])
+
+# named access = $ (pattern matching too)
+l$first
+l$f
+
+# nested element access
+l[["third"]]["beta"]
+l[["third"]][["beta"]]
+l[[c("third", "alpha")]]
+
+# bad indexing
+print("bad indexing")
+l[c(3, 2, 4)]
+l[c("fourth", "second", "fifth")]
+
+l[["fourth"]]
+l["fourth"]
+l$fourth
