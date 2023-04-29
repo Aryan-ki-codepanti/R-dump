c8c4eeaa13ff1e8ffac319bd9ce0d36be40a81ea
Aryan Sethi <aryan.rdps@gmail.com>
2023-04-29 15:38:47 +0530

    Added repeat

---
+++ ch 8/4_repeat_loop.R
@@ -0,0 +1,30 @@
+# Like do while
+# executes till told to stop
+# inf loop
+# repeat{
+#     print("message hey")
+# }
+
+# break in between using break
+repeat{
+    message("Happy Groundhog day")
+    action <- sample(
+        c("Learn 1" , "Rob" ,"Sleep"),1
+    )
+    message("action = ", action)
+    if(action == "Sleep") break
+}
+
+# skip to next iteration using next (like continue)
+repeat{
+    message("Happy Groundhog day")
+    action <- sample(
+        c("Learn 1" , "Rob" ,"Sleep"),1
+    )
+    if(action == "Rob") {
+        message("Skiipinggggg")
+        next
+    }
+    message("action = ", action)
+    if(action == "Sleep") break
+}
