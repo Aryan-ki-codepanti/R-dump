a384b40b65aa3017b293b4e1b65e2d18806041de
Aryan Sethi <aryan.rdps@gmail.com>
2023-04-29 15:30:05 +0530

    Added multi sel switch

---
+++ ch 8/3_multiple_selection.R
@@ -0,0 +1,50 @@
+
+greek <- switch(
+    "gamma",
+    alpha = 1,
+    beta = sqrt(-2),
+    gamma = {
+        a <- sin(pi/3)
+        4*a^2
+    }
+)
+
+greek
+# no match returns nulllllllll
+(greek <- switch(
+    "d",
+    alpha = 1,
+    beta = sqrt(-2),
+    gamma = {
+        a <- sin(pi/3)
+        4*a^2
+    }
+))
+
+# unnamed arg for no match
+(greek <- switch(
+    "d",
+    alpha = 1,
+    beta = sqrt(-2),
+    gamma = {
+        a <- sin(pi/3)
+        4*a^2
+    },
+    "default arg"
+))
+
+# switch with int match acc to pos
+switch(
+    3,
+    "first",
+    "second",
+    "third",
+    "d"
+)
+
+# big nums use characters lol
+switch(
+    as.character(123456),
+    "123456" = "big num",
+    "default"
+)
