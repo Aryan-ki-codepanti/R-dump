ed6e31256f5df7fb56a9d4779128a77e534bdefb
Aryan Sethi <aryan.rdps@gmail.com>
2023-04-29 15:23:37 +0530

    Added vectorized if

---
+++ ch 8/2_vectorized_if.R
@@ -0,0 +1,19 @@
+
+# if logical vec > 1 passed to if it takes 1st val only with warning
+
+# if(c(TRUE, FALSE))
+#     print("YO")
+
+# vectorized flow control ifelse()
+# if(0)
+#     print("gi")
+
+ifelse(rbinom(10, 1, 0.5), "Head", "Tail")
+
+yn <- rep.int(c(TRUE, FALSE), 6)
+ifelse(yn, 1:3, -1:-12)
+
+# missing vals -> missing res
+yn[c(3,6,9,12)] <- NA
+ifelse(yn, 1:3, -1:-12)
+
