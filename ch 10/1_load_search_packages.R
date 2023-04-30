f8958a818587a655befffb5390b89144561b46c6
Aryan Sethi <aryan.rdps@gmail.com>
2023-04-30 15:17:43 +0530

    added pkgs

---
+++ ch 10/1_load_search_packages.R
@@ -0,0 +1,32 @@
+# load with library()
+
+library(lattice)
+dotplot(
+    variety ~ yield | site,
+    data = barley
+)
+
+# load with char name
+pkgs <- c("lattice", "utils", "rpart")
+for (i in pkgs)
+    library(i, character.only = TRUE)
+
+# error
+# library(aryan)
+
+# better use , warns if not found else loads
+if(!require(aryan))
+    message("Aryan not exist")
+
+
+# see all loaded packages and envs
+search()
+
+# view installed packages
+# View(installed.packages())
+
+# lib
+R.home("library")
+
+
+.libPaths()
