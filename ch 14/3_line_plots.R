666484c2cd881a6e9118a621bdcb7da7877df2bc
Aryan Sethi <aryan.rdps@gmail.com>
2023-05-04 15:59:29 +0530

    added line plots

---
+++ ch 14/3_line_plots.R
@@ -0,0 +1,16 @@
+data(crab_tag, package = "learningr")
+
+# line plot with plot and type = "l"
+# crab_tag$daylog
+
+with(
+    crab_tag$daylog,
+    plot(Date, - Max.Depth, type = "l", ylim = c(-max(Max.Depth), 0), main = "CrabTagDS")
+)
+
+# additional lines on existing plot by lines() , for scatterplot is points
+with(
+    crab_tag$daylog,
+    lines(Date, - Min.Depth, col = "red")
+)
+
